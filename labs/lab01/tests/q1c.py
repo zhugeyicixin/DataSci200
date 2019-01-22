e2049b9da5a8517581e8401685445a17193b0ccd
papajohn <papajohn@gmail.com>
2019-01-21 20:11:14 -0800

    lab01

---
+++ labs/lab01/tests/q1c.py
@@ -0,0 +1,46 @@
+test = {
+  'name': 'q1c',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> average(1)
+          1.0
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> average(3)
+          2.0
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> average(8)
+          4.0
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> average(0)
+          3.0
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
