a6b6706e2b519f739f7a712d85734e5a67cdc1c5
data100.instructors <data100.instructors@berkeley.edu>
2019-02-10 15:08:53 -0800

    hw1, lab1, lab2 solutions

---
+++ hw/hw1/hw1_sol/tests/q2c.py
@@ -0,0 +1,14 @@
+test = {   'name': 'q2c',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': '>>> most in ["Joey", "Deb", '
+                                               '"Sam"]\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': ">>> most\n'Sam'",
+                                       'hidden': True,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
