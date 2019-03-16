068b6719c61046ec05af9e48df6ea4f12e6c0bce
data100.instructors <data100.instructors@berkeley.edu>
2019-03-15 17:23:25 -0700

    Lab 8 Release

---
+++ labs/lab08/tests/q3b.py
@@ -0,0 +1,14 @@
+test = {   'name': 'q3b',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': '>>> '
+                                               'np.isclose(l2(linear_model(revised_analytical_thetas, '
+                                               'one_hot_X_revised), '
+                                               'tips).mean(), 1.03328557, '
+                                               'rtol=1)\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
