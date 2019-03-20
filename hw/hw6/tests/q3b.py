7ee71d995cc3b1796e961ea019c603216fb15382
data100.instructors <data100.instructors@berkeley.edu>
2019-03-19 23:45:27 -0700

    Adding hw6.

---
+++ hw/hw6/tests/q3b.py
@@ -0,0 +1,16 @@
+test = {   'name': 'q3b',
+    'points': 2,
+    'suites': [   {   'cases': [   {   'code': '>>> 180220 <= y_fitted.mean() '
+                                               '<= 180225\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> 181430 <= '
+                                               'y_predicted.mean() <= 181440\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
