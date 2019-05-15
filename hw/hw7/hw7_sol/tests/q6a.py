ce080e8cd53bc1789d8ab0d130b6cb5c1be8dc95
data100.instructors <data100.instructors@berkeley.edu>
2019-05-15 11:18:41 -0700

    HW 7 Solutions

---
+++ hw/hw7/hw7_sol/tests/q6a.py
@@ -0,0 +1,17 @@
+test = {   'name': 'q6a',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': '>>> '
+                                               'isinstance(likeliness_to_use_ray, '
+                                               '(float, int))\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> 1 <= likeliness_to_use_ray '
+                                               '<= 10\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
